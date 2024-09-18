ab3b644af9d4a2d809e6f079d33fda24240517f8
Dani <d.maj.hmp@gmail.com>
2024-09-18 17:42:24 -0600

    update db for orders

--- add_order_info.sql
+++ add_order_info.sql
@@ -10,3 +10,18 @@ CREATE TABLE IF NOT EXISTS `orders` (
 ALTER TABLE `metaserver_users`
 ADD COLUMN `order_id` INT,
 ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+
+ALTER TABLE `orders`
+ADD COLUMN `order_info` VARCHAR(255)
+ADD COLUMN `owner_id` INT
+ADD FOREIGN KEY (`owner_id`) REFERENCES `metaserver_users` (`id`);
+
+CREATE TABLE IF NOT EXISTS `order_members` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `metaserver_users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
